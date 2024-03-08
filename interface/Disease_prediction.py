729b35b2af4ff7069e8a439d58be13ea48a43c20
hafilahaniya <143649116+hafilahaniya@users.noreply.github.com>
2024-03-09 02:27:23 +0530

    Update Disease_prediction.py

--- interface/Disease_prediction.py
+++ interface/Disease_prediction.py
@@ -13,11 +13,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open("D:/Multiple_Disease_Prediction/saved_models/diabetes_model.sav", 'rb'))
+diabetes_model = pickle.load(open("./saved_models/diabetes_model.sav", 'rb'))
 
-heart_disease_model = pickle.load(open("D:/Multiple_Disease_Prediction/saved_models/heart_disease_model.sav",'rb'))
+heart_disease_model = pickle.load(open("./saved_models/heart_disease_model.sav",'rb'))
 
-parkinsons_model = pickle.load(open("D:/Multiple_Disease_Prediction/saved_models/parkinsons_model.sav", 'rb'))
+parkinsons_model = pickle.load(open("./saved_models/parkinsons_model.sav", 'rb'))
 
 
 
